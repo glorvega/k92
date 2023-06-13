85d3612be8c89ec4adafc5d3c4834442fcaaa5e5
glorvega <gloria.vega@bootcamp-upgrade.com>
2023-06-13 21:25:17 +0200

    added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { CardComponent } from './shared/components/card/card.component';
+import { ArtistsComponent } from './pages/artists/artists.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    CardComponent,
+    ArtistsComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
